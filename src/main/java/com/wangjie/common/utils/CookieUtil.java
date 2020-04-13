d19f155c9307c1937c244707785793c50a263c4c
wangjie <1040312408@qq.com>
2020-04-14 07:14:37 +0800

    重新提交

---
+++ src/main/java/com/wangjie/common/utils/CookieUtil.java
@@ -0,0 +1,89 @@
+/**   
+ * Copyright © 2019 公司名. All rights reserved.
+ * 
+ * @Title: CookieUtil.java 
+ * @Prject: bobo-cms
+ * @Package: com.bobo.cms.controller 
+ * @Description: TODO
+ * @author: charles   
+ * @date: 2019年8月26日 下午6:24:59 
+ * @version: V1.0   
+ */
+package com.wangjie.common.utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 
+ * @ClassName: CookieUtil 
+ * @Description: TODO
+ * @author: wangjie
+ * @date: 2020年4月14日 上午7:07:30
+ */
+public class CookieUtil {
+/**
+ * 获取cookie
+ * @Title: getCookie 
+ * @Description: TODO
+ * @param request
+ * @param cookieName
+ * @return
+ * @return: Cookie
+ */
+	public static Cookie getCookie(HttpServletRequest request, String cookieName) {
+
+		Cookie cookie = null;
+		// 获取所有的cookie对象
+		Cookie[] cookies = request.getCookies();
+
+		if (cookies != null) {
+			for (Cookie ck : cookies) {
+				String name = ck.getName();
+				if (cookieName.equals(name)) {
+					cookie = ck;
+					break;
+				}
+			}
+		}
+
+		if (cookie == null) {
+			cookie = new Cookie(cookieName, null);
+		}
+
+		return cookie;
+	}
+	
+	//检查是否存在该cookie
+	public static boolean isCheck(Cookie cookie,String newValue,HttpServletResponse response) {
+		if (cookie.getValue() != null) {
+			String[] split = cookie.getValue().split("-");
+			if (split != null && split.length > 0) {
+				List<String> asList = Arrays.asList(split);
+				if (asList.contains(newValue )) {
+					System.out.println("已经有了");
+					return false;
+				} else {
+					//设置新的cookie值
+					cookie.setValue(newValue);
+					//回写到浏览器  
+					response.addCookie(cookie);
+					
+					
+					System.out.println("没有");
+					return true;
+				}
+			}
+		}else {
+			//设置新的cookie值
+			cookie.setValue(newValue);
+			//回写到浏览器  
+			response.addCookie(cookie);
+		}
+		return true;
+	}
+}
